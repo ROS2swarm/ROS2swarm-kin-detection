60463cf2c91766c02dc5d8a7f1b1cf4acd5d7288
jonathan-h1 <37294529+jonathan-h1@users.noreply.github.com>
2024-02-20 13:29:02 +0100

    add new mobilenet model

--- ml-model/model.py
+++ ml-model/model.py
@@ -14,7 +14,7 @@ def conv_layer(ch_in, ch_out):
         torch.nn.BatchNorm1d(ch_in),
         CloseRing(),
         torch.nn.Conv1d(ch_in, ch_out, 3),
-        torch.nn.ReLU()
+        torch.nn.Hardswish()
     )
     return layer
 
@@ -22,22 +22,31 @@ class SimpleModel(torch.nn.Module):
 
     def __init__(self, config):
         super().__init__()
+        self.channels = config.LAYERS
 
         tmp_layers = []
         for l in range(len(config.LAYERS) - 1):
             tmp_layers.append(conv_layer(config.LAYERS[l], config.LAYERS[l + 1]))
 
-        self.layers = torch.nn.Sequential(*tmp_layers)
+        self.layers = torch.nn.ModuleList(tmp_layers)
 
     def forward(self, x):
-        return self.layers(x)
+        xout = x
+        for i, l in enumerate(self.layers):
+            xoutn = l(xout)
+            if i + 1 == len(self.layers) or self.channels[i + 1] != self.channels[i + 2]:
+                xout = xoutn
+            else:
+                xout = xout + xoutn
+        return xout
 
 
 def build_model(config):
-    if config.NAME == 'simple':
+    if config.NAME.startswith('simple'):
         return SimpleModel(config)
-    elif config.NAME == 'mobile':
-        mobile_layer_conf, _ = _mobilenet_v3_conf('mobilenetPts_v3_small')
+    elif config.NAME.startswith('mobile'):
+        net_postfix = config.NAME.replace('mobile', '')
+        mobile_layer_conf, _ = _mobilenet_v3_conf(f'mobilenetPts_v3{net_postfix}')
         return MobileNetV3(mobile_layer_conf)
     else:
         print(f"No model {config.NAME} implemented")
